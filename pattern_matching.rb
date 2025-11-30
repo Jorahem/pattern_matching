62baf8a738082dffcd0b78d3179b67dcae3816b9
jorahem <shahnero12@gmail.com>
2025-11-30 15:07:09 +0545

    done

---
+++ pattern_matching.rb
@@ -0,0 +1,39 @@
+grade = 'C'
+
+result = case grade
+  in 'A' then 1
+  in 'B' then 2
+  in 'C' then 3
+  else 0
+end
+
+puts result
+
+
+login = { username: 'hornby', password: 'iliketrains' }
+
+login => { username: username }
+
+puts "Logged in with username #{username}"
+
+
+
+input = 3
+
+case input
+in String then puts 'input was of type String'
+in Integer then puts 'input was of type Integer'
+end
+
+
+
+
+age = 15
+
+case age
+in a
+  puts a
+end
+
+
+
